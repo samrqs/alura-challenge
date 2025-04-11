ac106850c470bb064cf5900de36209c6717a1432
Samara Marques <samara.souza@scicrop.com>
2025-04-11 17:54:05 -0300

    fix: add created_at in models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 import uuid
 
+from datetime import datetime
+
 from .extensions import db
 
 from sqlalchemy.dialects.postgresql import UUID
@@ -9,11 +11,13 @@ class Feedback(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     feedback_text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
         return {
             "id": self.id,
             "feedback_text": self.feedback_text,
+            "created_at": self.created_at.isoformat()
         }
 
 class Classification(db.Model):
@@ -23,7 +27,7 @@ class Classification(db.Model):
     sentiment = db.Column(db.String, nullable=False)
     code = db.Column(db.String, nullable=False)
     reason = db.Column(db.Text, nullable=False)
-
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     feedbacks_id = db.Column(UUID(as_uuid=True), db.ForeignKey('feedbacks.id'), nullable=False)
 
@@ -32,5 +36,6 @@ class Classification(db.Model):
             "id": str(self.id),
             "code": self.code,
             "reason": self.reason,
-            "feedbacks_id": str(self.feedbacks_id)
+            "feedbacks_id": str(self.feedbacks_id),
+            "created_at": self.created_at.isoformat()
         }
