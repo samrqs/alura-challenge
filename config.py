54722316a0962d310b2d13138336e399aeec3885
Samara Marques <103200488+samrqs@users.noreply.github.com>
2025-04-12 15:25:57 -0300

    update config.py

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config:
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'True') == 'True'
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
